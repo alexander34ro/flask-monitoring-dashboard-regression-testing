f3474eee48445ae4f86b8dbdc432cb4a97247b8c
Alex Ardelean <ardeleanalexandru3@gmail.com>
2020-11-26 00:12:29 +0200

    Raise performance impact

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,12 @@
-# Get pip3
+# Update apt
 sudo apt-get update
+
+# Get pip3
 sudo apt-get install python3-pip
 pip3 install -r requirements.txt
+
+# Get gunicorn
+sudo apt-get install gunicorn
 
 # Launch
 gunicorn wsgi:app --bind 0.0.0.0:8000
