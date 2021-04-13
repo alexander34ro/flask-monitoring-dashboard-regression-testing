9b0948bf6aba9f81ec0a1e3bcd79e3ffef210fce
Alex Ardelean <ardeleanalexandru3@gmail.com>
2021-04-13 17:03:02 +0300

    Minor improvements

--- changed_functions.sh
+++ changed_functions.sh
@@ -1,7 +1,10 @@
-git show $1^..HEAD |
-grep -E '^(@@)' |
-sed 's/@@.*@@//' |
-sed 's/def //' |
-sed '/^$/d' |
-sort |
+git show $1^..HEAD  |
+grep -E '^(@@)'     |
+grep -v 'import '   |
+sed 's/@@.*@@//'    |
+sed 's/def //'      |
+sed 's/://'         |
+sed 's/ //'         |
+sed '/^$/d'         |
+sort                |
 uniq
